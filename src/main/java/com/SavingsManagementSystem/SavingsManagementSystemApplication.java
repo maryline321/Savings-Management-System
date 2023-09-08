c7b5c8ccd677fdc079bdab42efe1f6baedbe7be0
Chebet Maryline <77463649+maryline321@users.noreply.github.com>
2023-09-08 11:28:28 +0300

    initial commit

---
+++ src/main/java/com/SavingsManagementSystem/SavingsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.SavingsManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsManagementSystemApplication.class, args);
+	}
+
+}
